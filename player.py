2174ec77d3f483f5d6a367236f92e0413b39bcf0
JuLiaN47V <46824535+JuLiaN47V@users.noreply.github.com>
2023-04-14 11:13:04 +0200

    Update player.py

--- player.py
+++ player.py
@@ -3,6 +3,8 @@ class Player:
     VERSION = "Schmetter 0.0.1"
 
     def betRequest(self, game_state):
+        if game_state["current_buy_in"] > 500:
+            return 0
         return (game_state["current_buy_in"] - game_state["players"][game_state["in_action"]]["bet"]) + 10
 
     def showdown(self, game_state):
