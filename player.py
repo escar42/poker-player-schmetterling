47535b00f1f383d00599597fb08e56f11517c812
JuLiaN47V <46824535+JuLiaN47V@users.noreply.github.com>
2023-04-14 10:32:55 +0200

    Update player.py

--- player.py
+++ player.py
@@ -4,7 +4,7 @@ class Player:
 
     def betRequest(self, game_state):
         players[in_action][bet] = current_buy_in + 10
-        return 0
+        return current_buy_in - players[in_action][bet]
 
     def showdown(self, game_state):
         pass
