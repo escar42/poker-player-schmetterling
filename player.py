0b165c900929be5c26729a223a987ef0bd42d6b0
JuLiaN47V <46824535+JuLiaN47V@users.noreply.github.com>
2023-04-14 11:23:12 +0200

    Update player.py

--- player.py
+++ player.py
@@ -3,9 +3,14 @@ class Player:
     VERSION = "Schmetter 0.0.1"
 
     def betRequest(self, game_state):
-        if game_state["current_buy_in"] > 500:
+        cards = game_state["players"][game_state["in_action"]]["hole_cards"]
+        card1 = cards[0]
+        card2 = cards[1]
+        if card1["suit"] == card2["suit"]:
+            return game_state["players"][game_state["in_action"]]["stack"]
+        elif game_state["current_buy_in"] > 500:
             return 0
-        return (game_state["current_buy_in"] - game_state["players"][game_state["in_action"]]["bet"]) + 10
+        return (game_state["current_buy_in"] - game_state["players"][game_state["in_action"]]["bet"])
 
     def showdown(self, game_state):
         pass
