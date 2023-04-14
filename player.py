2d53b1cbc2ee1282bc52bae995cacbfb302763a3
JuLiaN47V <46824535+JuLiaN47V@users.noreply.github.com>
2023-04-14 11:49:29 +0200

    Update player.py

--- player.py
+++ player.py
@@ -7,12 +7,14 @@ class Player:
         card1 = cards[0]
         card2 = cards[1]
         comcards_suits = []
+        comcards_ranks = []
         for comcard_suits in game_state["community_cards"]:
             comcards_suits.append(comcard_suits["suit"])
+            comcards_ranks.append(comcard_suits["rank"])
         
         if card1["suit"] == card2["suit"] or card1["suit"] in comcards_suits or card2["suit"] in comcards_suits:
             return game_state["players"][game_state["in_action"]]["stack"]
-        elif game_state["current_buy_in"] > 500:
+        elif game_state["current_buy_in"] > 200:
             return 0
         return (game_state["current_buy_in"] - game_state["players"][game_state["in_action"]]["bet"])
 
