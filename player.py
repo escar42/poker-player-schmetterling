acbb20aeb5c2c27a5ee1a24d380a12c88c00ca19
JuLiaN47V <46824535+JuLiaN47V@users.noreply.github.com>
2023-04-14 10:24:13 +0200

    Update player.py

--- player.py
+++ player.py
@@ -3,6 +3,7 @@ class Player:
     VERSION = "Default Python folding player"
 
     def betRequest(self, game_state):
+        players[in_action][bet] = current_buy_in + 10
         return 0
 
     def showdown(self, game_state):
